d0e8eaf3c221c6390452b28dc731e675deb1452a
MissKN <165464075+MissKN@users.noreply.github.com>
2025-07-18 11:08:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const AUTHOR_NAME = 'Nuno Marques'
 export const SITE_URL = 'https://doodlesgames.netlify.app/'
-export const SITE_NAME = 'doodlesgames.netlify.app'
+export const SITE_NAME = 'doodlesgames'
 export const SHOW_PER_PAGE = 10
